c8bc6dfa25fb9812d8927f88d9e3dcd8504a439f
Xavier Laviron <xavier.laviron@gmx.fr>
2017-03-01 07:23:28 +0100

    clean code of main script
    I added code in main script to source the 'clean_values' script.

--- load_data.R
+++ load_data.R
@@ -21,7 +21,6 @@ if (!(file.exists("data/converted_data.csv"))) {
 
 converted_data <- read.csv("data/converted_data.csv",
                            stringsAsFactors = FALSE,
-                           dec = ",",
                            encoding = "utf8")
 other_data <- read.csv("data/BDD_dec2016_culture.csv",
                        stringsAsFactors = FALSE,
@@ -29,18 +28,18 @@ other_data <- read.csv("data/BDD_dec2016_culture.csv",
                        encoding = "utf8")
 
 # merge the 2 databases into one data.frame
-data_full <- rbind.data.frame(other_data, converted_data)
+data_full_tmp <- rbind.data.frame(other_data, converted_data)
 
 # remove trailing and leading whitespace from dataframe
-data_full <-
-  data_full %>%
+data_full_tmp <-
+  data_full_tmp %>%
   mutate_all(.funs = trimws)
 
-data_full$Protection_visée[data_full$Produit_phyto == "KARATE K"] <- "insecticide"
-data_full$Produit_phyto[data_full$Produit_phyto == "U46D"] <- "U 46 D"
-
 # write it to a file
-write.csv(data_full, "data/BDD_full.csv", row.names = FALSE)
+write.csv(data_full_tmp, "data/BDD_full_tmp.csv", row.names = FALSE)
+
+source("clean_values.R")
+data_full <- read.csv("data/BDD_full.csv", stringsAsFactors = FALSE)
 
 # ------------------------------------------------------------------------------
 
@@ -48,5 +47,5 @@ write.csv(data_full, "data/BDD_full.csv", row.names = FALSE)
 cat("Conversion et subsetting du fichier enquêtes...\n")
 source("util/load_enquete.R", encoding = "latin1")
 
-source("util/R_pesticides.R", encoding = "latin1")
-ift_herbi <- Intensite_Traitement()
+# source("util/R_pesticides.R", encoding = "latin1")
+# ift_herbi <- Intensite_Traitement()
