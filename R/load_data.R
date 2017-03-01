918c1fedce413cd24023cdadbdb2706fe0348821
Xavier Laviron <xavier.laviron@gmx.fr>
2017-03-01 20:37:26 +0100

    adds computation for the nitrogen dose
    The dose can be calculated from a reference table -> there are not a lot of
    products in this table so it is not very useful at the time.

--- R/load_data.R
+++ R/load_data.R
@@ -5,8 +5,7 @@
 # ------------------------------------------------------------------------------
 
 # -- packages ------------------------------------------------------------------
-library(plyr)
-library(dplyr)
+library(tidyverse)
 
 # -- data management -----------------------------------------------------------
 # loads and converts the data
@@ -15,15 +14,15 @@ library(dplyr)
 
 # loading
 cat("Chargement des données...\n")
-if (!(file.exists("data/converted_data.csv"))) {
+if (!(file.exists("data/generated/converted_data.csv"))) {
   # if not done already
   source("db_convert.R")
 }
 
-converted_data <- read.csv("data/converted_data.csv",
+converted_data <- read.csv("data/generated/converted_data.csv",
                            stringsAsFactors = FALSE,
                            encoding = "utf8")
-other_data <- read.csv("data/BDD_dec2016_culture.csv",
+other_data <- read.csv("data/raw/BDD_dec2016_culture.csv",
                        stringsAsFactors = FALSE,
                        dec = ",",
                        encoding = "utf8")
@@ -39,20 +38,74 @@ data_full_tmp <-
   data_full_tmp %>%
   mutate_all(.funs = trimws)
 
-
 # write it to a file
-write.csv(data_full_tmp, "data/BDD_full_tmp.csv", row.names = FALSE)
+write.csv(data_full_tmp, "data/generated/BDD_full_dirty.csv", row.names = FALSE)
 
 cat("Nettoyage des valeurs...\n")
 source("clean_values.R")
 
-data_full <- read.csv("data/BDD_full.csv", stringsAsFactors = FALSE)
+data_full <- read.csv("data/generated/BDD_full.csv", stringsAsFactors = FALSE)
+
+# -- subsetting du jeu de données ----------------------------------------------
+cat("Subsetting...\n")
+# récupération des plots contenant des relevés de flore
+data_sub <-
+  data_full %>%
+  filter(ID_Enquêteur %in% c("inchausti", "grison"),
+         Année_Enquête %in% c(2007, 2011))
+
+data_sub$Produit_Ferti <-
+  data_sub$Produit_Ferti %>%
+  stri_replace_all(fixed = "/", repl = "-")
+
+data_sub$id_parc_tri <- paste(data_sub$ID_Exploitation,
+                              data_sub$ID_Parcelle,
+                              data_sub$Année_SuiviParcelle,
+                              sep = "_")
+
+# reads the reference table for fertilizers
+fert <- read.csv("data/raw/BD_Fertilizers_dec2016.csv", stringsAsFactors = FALSE)
+fert$Fertilisant  <- toupper(trimws(fert$Fertilisant))
+colnames(fert)[2] <- "kg_n_100kg"
+
+# calcul de la dose de fertilisants à partir de la concentration lue dans le
+# tableau de référence
+# compute reference dose from concentration found in the ref table
+n_ha <- numeric(nrow(data_sub))
+n_ha[n_ha == 0] <- NA
+
+for (i in 1:nrow(data_sub)) {
+  if (is.na(data_sub$Unité_dose[i])) {
+    next
+  }
+  if (data_sub$Unité_dose[i] == "Unité/HA") {
+    # 1 U/HA = 1 Kg/HA
+    n_ha[i] <- data_sub$Dose_Ferti[i]
+  }
+  else if (data_sub$Unité_dose[i] == "Kg/HA") {
+    if (toupper(data_sub$Produit_Ferti[i]) %in% fert$Fertilisant) {
+      # m_n_100 is the nitrogen mass for 100 Kg of product, found in the
+      # reference table
+      m_n_100 <- fert$kg_n_100kg[fert$Fertilisant == toupper(data_sub$Produit_Ferti[i])] 
+      n_ha[i] <- data_sub$Dose_Ferti[i] * m_n_100 / 100
+    }
+  }
+}
+
+# adds the new column to the df
+data_sub$kg_n_ha <- n_ha
+
+# adds column with the yield in qtx / Ha
+data_sub$Rdt_Qtx <- as.numeric(data_sub$Rdt_Qtx)
+data_sub$qtx_ha <- data_sub$Rdt_Qtx / as.numeric(data_sub$Surface_ha)
+
+write.csv(data_sub, "data/generated/BDD_sub.csv", row.names = FALSE)
 
 # ------------------------------------------------------------------------------
 
 # formatting and subsetting the data.frame with Sabrina's scripts
-cat("Conversion et subsetting du fichier enquêtes...\n")
-source("util/load_enquete.R", encoding = "latin1")
+# cat("Conversion et subsetting du fichier enquêtes...\n")
+# source("util/load_enquete.R", encoding = "latin1")
 
 # source("util/R_pesticides.R", encoding = "latin1")
 # ift_herbi <- Intensite_Traitement()
