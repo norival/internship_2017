3c44a0f7f0c97283ebca84eca9f5f7c5587a9907
Xavier Laviron <xavier.laviron@gmx.fr>
2017-03-14 14:37:20 +0100

    adds optimized function to code2011

--- R/util/code2011.R
+++ R/util/code2011.R
@@ -18,7 +18,7 @@ var <- colnames(data2011)
 
 unique(data2011$Crop.Analyses) #céréale/Cereal, colza/Colza,Maïs/maïs
 levels(data2011$Crop.Analyses)=c("cereal")
-data2011$Espèce_origin[data2011$Espèce_origin=="Festuca-ovina/rubra"]="Festuca-rubra" #Festuca-ovina/rubra
+data2011$Espece_origin[data2011$Espece_origin=="Festuca-ovina/rubra"]="Festuca-rubra" #Festuca-ovina/rubra
 
 ###Correction des noms d'espèces adventices
 ##code Joël
@@ -35,7 +35,7 @@ data2011=modifs_fichier(tab=data2011)
 #############################################################
 
 # On conserve Year, date, Nb_parcelle, Par, Par.interf, 
-#Crop.Analyses, pt, lat, LONG, Espèce_origin, abondance, ParPoint
+#Crop.Analyses, pt, lat, LONG, Espece_origin, abondance, ParPoint
 kept <-  c(3,4,6,7,8,9,11,12,13,14,22,31)
 
 weeds2011 <- data2011[, kept ]
@@ -72,10 +72,10 @@ weeds2011C$Par.interf[weeds2011C$Par.interf=="Pa"]="pa"
 ###length(unique(weeds2011C$Crop.Analyses))
 ## 5 cultures
 
-###length(unique(weeds2011$Espèce_origin))
+###length(unique(weeds2011$Espece_origin))
 ## 294 espèces soit 294 colonnes dans la matrice
 
-weeds2011C1 <- cbind(weeds2011C, as.factor(weeds2011C$Espèce_origin))
+weeds2011C1 <- cbind(weeds2011C, as.factor(weeds2011C$Espece_origin))
 colnames(weeds2011C1) [13] <- "sp"
 
 write.table(weeds2011C1, "data/generated/weeds2011.csv", sep = ";")
@@ -277,66 +277,13 @@ write.table(A_Diversity, "data/generated/Diversity_fieldcore2011.csv", sep = ";"
 # -- estimation des abondances -------------------------------------------------
 
 # package
-library(compoisson)
+source("functions/abundance.R", encoding = "utf8")
 
 # on choppe les donnees
-data2011.dat <- read.csv("data/generated/transpose_abondance_per_quadrat2011.csv",
-                         sep = ";", stringsAsFactors = FALSE, encoding = "utf8")
+data2011 <- read.csv("data/generated/transpose_abondance_per_quadrat2011.csv",
+                     sep = ";", stringsAsFactors = FALSE, encoding = "utf8")
 
-# calcule la vraissemblance des données des quadras
-# v = observations
-# ltheta = log du paramètre de poisson (intensité)
-h.fct <- function(ltheta,v=v) {
-  # print(c(ltheta, v))
-  theta <- exp(ltheta)
-
-  # si param est énorme, pas possible
-  if(max(theta)>30){return(100000)}
-
-  # proba d'abondance pour les espèces ayant un indice d'abondance de 0
-  lp0 <- com.log.density(0,theta[1],theta[2])
-  # proba d'abondance pour les espèces ayant un indice d'abondance de 1
-  lp1 <- com.log.density(1,theta[1],theta[2])
-  # proba d'abondance pour les espèces ayant un indice d'abondance de 2
-  lp2 <- log(1-exp(lp0)-exp(lp1))
-  lp <- c(lp0,lp1,lp2)
-  ll <- (-1)*sum(lp[v+1])
-  return(ll)
-}
-
-# Création d'un tableau vide pour récupérer les estmations d'abondances
-mat_vide <- matrix(Inf,
-                   ncol = length(unique(data2011.dat$sp)),
-                   nrow = length(unique(data2011.dat$carre.parc)))
-abond_per_plot <- as.data.frame(mat_vide)
-
-colnames(abond_per_plot) <- unique(data2011.dat$sp)
-rownames(abond_per_plot) <- unique(data2011.dat$carre.parc)
-
-for (parc in unique(data2011.dat$carre.parc)) {
-
-  # On récupère les lignes pour la parcelle parc
-  dat_sub <- data2011.dat[data2011.dat$carre.parc == parc, ]
-
-  # ab <- NULL
-  for (i in (1:nrow(dat_sub))) {
-    # param de Poisson par espèce
-    # ici on récupère la ligne i, qui correspond à un espèce pour la parelle
-    # parc
-    v1 <- as.numeric(dat_sub[i, 4:ncol(dat_sub)])
-
-    # on estime l'abondance de la parcelle par la loi de poisson
-    Zu <- nlminb(c(0, 0), h.fct, v = v1, lower = c(-50, -50),upper = c(50, 50))
-
-    # on fait la moyenne de poisson sur le paramètre de Zu, qui correspond aux
-    # moyennes. On repasse en exponentielle car on avait fait un log
-    mm <- com.mean(exp(Zu$par[1]), exp(Zu$par[2]))
-
-    # On rajoute cette moyenne dans le tableau vide initial
-    abond_per_plot[parc, dat_sub$sp[i]] <- mm
-
-  }
-}
+abond_per_plot <- estim_abundance(data2011, surf = 4, n_cores = 4)
 
 write.csv(abond_per_plot, "data/generated/abondt_per_plot_2011.csv",
           row.names = FALSE)
