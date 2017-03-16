08392b2de8cef51baebf2cf7d659fb16f5062b2f
Xavier Laviron <xavier.laviron@gmx.fr>
2017-03-16 16:01:46 +0100

    complete summary for flora results

--- R/flora_summary.R
+++ R/flora_summary.R
@@ -11,44 +11,115 @@ for (file in files) {
 }
 
 
-# ------------------------------------------------------------------------------
-# 2006
-a <- weeds2006
+# -- all plants ----------------------------------------------------------------
+summary_flora <- function() {
+  sum_plots <- data.frame(year        = numeric(0),
+                          n_plots     = numeric(0),
+                          n_sp        = numeric(0),
+                          n_sp_pplot  = numeric(0),
+                          mab_pplot   = numeric(0))
+  sum_flora <- data.frame(year      = numeric(0),
+                          sp        = character(0),
+                          total     = numeric(0))
 
-# removes interface for which abundance have not been estimated
-a <- a[!grepl("-In", rownames(a)),]
+
+  for (year in (2006:2016)[-which(2006:2016 == 2012)]) {
+    a <- get(paste("weeds", year, sep = ""))
+
+    if (year <= 2011) {
+      # removes interface for which abundance have not been estimated
+      a <- a[!grepl("-In", rownames(a)),]
+    }
+
+    # replace very small values by 0
+    a[a <= 3.5e-09] <- 0
+
+    # remove species that are not present
+    a <- a[, apply(a, 2, sum) != 0]
+
+    # number of species
+    n_sp <- ncol(a)
+
+    # nb plots
+    n_plots <- length(unique(rownames(a)))
+
+    # nb species per plot
+    n_sp_pplot <- mean(apply(a, 1, function(x) sum(x != 0)))
+
+    # mean abundance per plot
+    mab_pplot <- mean(apply(a, 1, mean))
+
+    # present species
+    sp <- colnames(a)
+
+    # sum per species
+    total <- as.numeric(apply(a, 2, sum))
+
+    # fill the dataframe
+    tab <- cbind(year, n_plots, n_sp, n_sp_pplot, mab_pplot)
+    sum_plots <- rbind.data.frame(sum_plots, tab)
+
+    tab <- cbind.data.frame(year, sp, total)
+    sum_flora <- rbind.data.frame(sum_flora, tab)
+  }
+
+  return(list(sum_plots, sum_flora))
+}
+
+a <- summary_flora()
+sum_plots <- a[[1]]
+sum_flora <- a[[2]]
+
+sum_flora$total <- as.numeric(as.character(sum_flora$total))
+
+sum_flora <-
+  sum_flora %>%
+  group_by(sp) %>%
+  summarise(total = sum(total))
+
+species <- character(0)
+parc    <- character(0)
+for (year in (2006:2016)[-which(2006:2016 == 2012)]) {
+  a <- get(paste("weeds", year, sep = ""))
+
+  if (year <= 2011) {
+    # removes interface for which abundance have not been estimated
+    a <- a[!grepl("-In", rownames(a)),]
+  }
+
+ species <- c(species, colnames(a)) 
+ parc <- c(parc, rownames(a))
+}
+
+mat <-
+  as.data.frame(matrix(0, nrow = length(unique(parc)), ncol = length(unique(species))),
+                stringsAsFactor = FALSE)
+rownames(mat) <- unique(parc)
+colnames(mat) <- unique(species)
+
+for (year in (2006:2016)[-which(2006:2016 == 2012)]) {
+  a <- get(paste("weeds", year, sep = ""))
+
+  if (year <= 2011) {
+    # removes interface for which abundance have not been estimated
+    a <- a[!grepl("-In", rownames(a)),]
+  }
+
+  for (i in 1:nrow(a)) {
+    for (ii in 1:ncol(a)) {
+      parc <- rownames(a)[i]
+      sp <- colnames(a)[ii]
+      mat[parc, sp] <- a[i, ii]
+    }
+  }
+}
+
 
 # replace very small values by 0
-a[a <= 3.5e-09] <- 0
+mat[mat <= 3.5e-09] <- 0
+occurence <- apply(mat, 2, function(x) sum(x != 0) / length(x) * 100)
 
-# remove species that are not present
-a <- a[, apply(a, 2, sum) != 0]
-
-# nb plots
-plots <- unique(gsub("-2006-Pa", "", rownames(a)))
-n_plots <- length(plots)
-
-# present species
-sp <- colnames(a)
-
-# nb species per plot
-mean(apply(a, 1, function(x) sum(x != 0)))
-
-# percentage of presence per species
-perc_sp <- apply(a, 2, function(x) sum(x != 0) / length(x) * 100)
-length(perc_sp[perc_sp >= 50])
-perc_sp[perc_sp >= 25 & perc_sp < 50]
-
-`%incl%` <- function(x, range)  {
-  res <- ifelse(x >= range[1] & x < range[2],
-                TRUE,
-                FALSE)
-  return(res)
+sum_flora$occurence <- NA
+for (i in 1:nrow(sum_flora)) {
+  sum_flora$occurence[i] <- occurence[sum_flora$sp[i]]
 }
-23 %incl% c(24, 40)
-
-# classify species by percentage:
-#   [25;100] -> abundant
-#   [5;25[   -> intermediate
-#   [0;5[    -> rare
-a$status <- ""
