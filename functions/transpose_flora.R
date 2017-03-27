44bc07579f80f0056beca31a4180fedb577db9ef
Xavier Laviron <xavier.laviron@gmx.fr>
2017-03-27 19:16:44 +0200

    simplify construction of matrix

--- functions/transpose_flora.R
+++ functions/transpose_flora.R
@@ -7,15 +7,11 @@ transpose_flora <- function(tab, base = 0) {
     tab$Note[tab$Note > 2] <- 2
 
   # Prepare an empty matrix filled with 0 (for 0 abundance observed)
+  names <- unique(paste(tab$Zone, tab$Quadra, tab$Placette, sep = ""))
   nrowA <- length(unique(tab$Parcelle)) * length(unique(tab$Taxon))
-  ncolA <- 2+8*4*4
-  A <- matrix(ncol = ncolA, nrow=nrowA , data = rep(0, ncolA*nrowA ))
+  ncolA <- length(names) + 2
+  A <- matrix(ncol = ncolA, nrow = nrowA , data = rep(0, ncolA*nrowA ))
   A <- data.frame(A)
-
-  zones <- rep(paste("Z", 1:8, sep = ""), rep(16, 8))
-  quadr <- rep(paste("Q", 1:4, sep = ""), rep(4, 4))
-  subqd <- rep(letters[1:4], 4)
-  names <- paste(zones, quadr, subqd, sep = "")
 
   colnames(A) <- c("sp", "carre.parc", names)
 
@@ -40,10 +36,6 @@ transpose_flora <- function(tab, base = 0) {
 
     A[A$sp == sp & A$carre.parc == field, colnames(A) == code] <- abondance
   }
-
-  # remove zones that are not present in the dataset
-  zones_in <- unique(paste(tab$Zone, tab$Quadra, tab$Placette, sep = ""))
-  A <- A[, colnames(A) %in% c("sp", "carre.parc", zones_in)]
 
   return(A)
 
