244705871594955542f73de13b4b467d910d09e5
Xavier Laviron <xavier.laviron@gmx.fr>
2017-03-28 14:58:17 +0200

    adds some comments and simplifications to 'verif_ab'

--- R/verif_ab.R
+++ R/verif_ab.R
@@ -23,17 +23,19 @@ surf <- 0.25
 
 dat <- as.data.frame(matrix(nrow = 0, ncol = 6, 0))
 for (per in 1:3) {
-  # per <- 1
-  # base <- 0
   cat("Computing period ", per, "...\n", sep = "")
   tab <- flore[flore$Periode == per,]
 
   for (base in c(0, 2)) {
     cat(" Computing base ", base, "...\n", sep = "")
-    tabt <- transpose_flora(tab, base = base)
-    origin <- tabt[["origin"]]
-    conv   <- tabt[["converted"]]
+    # transpose the dataframe into the same format than the monitoring
+    # dataframes so the functions to estimate abundances can be used
+    tabt    <- transpose_flora(tab, base = base)
+    origin  <- tabt[["origin"]]
+    conv    <- tabt[["converted"]]
 
+    # use the appropriate function to estimate the abundance of converted
+    # dataframes, according to 'base'
     if (base == 0) {
       ab <- estim_abundance01(conv, surf, progress = FALSE)
     } else {
@@ -44,7 +46,11 @@ for (per in 1:3) {
     estim$periode <- paste("period", per, sep = "")
     estim$base    <- paste("base", base, sep = "")
 
+    # removes infinite values to compute a linear model on the data
     estim_noinf <- estim[!(is.infinite(estim$esti)),]
+
+    # fit a linear model on the data, and get the R^2 and the equation of the
+    # model to print those on the graphics
     mod <- lm(esti ~ real,
               data = estim_noinf[estim_noinf$periode == paste("period", per, sep = "") &
                                  estim_noinf$base == paste("base", base, sep = ""),])
@@ -57,11 +63,13 @@ for (per in 1:3) {
 }
 dat_noinf <- dat[!(is.infinite(dat$esti)),]
 
+# aggregate labels to be displayed on the plot
 f_labels <- aggregate(data.frame(r2 = dat$r2, eqn = dat$eqn),
                       list(periode = dat$periode, base = dat$base),
                       unique)
 f_labels$r2 <- paste("R2=", round(f_labels$r2, 3), sep = "")
 
+# plot dat!
 p <- ggplot(dat_noinf, aes(x = real, y = esti)) +
   geom_point(size = 2) +
   geom_abline(slope = 1, intercept = 0) +
