810850572c28b940fe87d2c72e5659ac59f8a462
Xavier Laviron <xavier.laviron@gmx.fr>
2017-03-28 09:45:31 +0200

    adds loop to 'verif_ab' to compute all periods
    The 3 periods and the 2 bases are computed inside the loop and store in one
    dataframe.
    Also adds models + plots

--- verif_ab.R
+++ verif_ab.R
@@ -20,39 +20,56 @@ flore <- rbind(flore1, flore2, flore3)
 # The values are converted whether in base 0 (0/1) or base2 and then the
 # abundance is estimated like for the other samples.
 
-tab <- flore[flore$Periode == 2,]
-surf = 0.25
+dat <- as.data.frame(matrix(nrow = 0, ncol = 6, 0))
+for (per in 1:3) {
+  # per <- 1
+  # base <- 0
+  cat("Computing period ", per, "...\n", sep = "")
+  tab <- flore[flore$Periode == per,]
 
-# Check when 0/1 notes
-tab0        <- transpose_flora(tab, base = 0)
-tab0_estim  <- estim_abundance01(tab0, surf = 0.25, progress = FALSE)
-dat0        <- estim_summary(tab0, tab0_estim, surf)
+  for (base in c(0, 2)) {
+    cat(" Computing base ", base, "...\n", sep = "")
+    tabt <- transpose_flora(tab, base = base)
+    origin <- tabt[["origin"]]
+    conv   <- tabt[["converted"]]
 
-ggplot(dat0, aes(x = real, y = esti)) +
+    if (base == 0) {
+      ab <- estim_abundance01(conv, surf, progress = FALSE)
+    } else {
+      ab <- estim_abundance(conv, surf, n_cores = 4, progress = FALSE)
+    }
+
+    estim <- estim_summary(origin, ab, surf)
+    estim$periode <- paste("period", per, sep = "")
+    estim$base    <- paste("base", base, sep = "")
+
+    estim_noinf <- estim[!(is.infinite(estim$esti)),]
+    mod <- lm(esti ~ real,
+              data = estim_noinf[estim_noinf$periode == paste("period", per, sep = "") &
+                                 estim_noinf$base == paste("base", base, sep = ""),])
+    estim$r2 <- summary(mod)$adj.r.squared
+    mod$coef <- round(mod$coef, 3)
+    estim$eqn <- paste("Y = ", mod$coef[2], "X + ", mod$coef[1], sep = "")
+
+    dat <- rbind.data.frame(dat, estim)
+  }
+}
+dat_noinf <- dat[!(is.infinite(dat$esti)),]
+
+f_labels <- aggregate(data.frame(r2 = dat$r2, eqn = dat$eqn),
+                      list(periode = dat$periode, base = dat$base),
+                      unique)
+f_labels$r2 <- paste("R2=", round(f_labels$r2, 3), sep = "")
+
+p <- ggplot(dat_noinf, aes(x = real, y = esti)) +
   geom_point(size = 2) +
-  xlim(0, 2) +
-  ylim(0, 2) +
-  ggtitle("Estimations Vs. Réelles pour relevés 0/1") +
-  geom_abline(slope = 1, intercept = 0)
-ggsave("~/desktop/graphbase0.pdf")
-
-
-# Check with base2 estimates
-tab2        <- transpose_flora(tab, base = 2)
-tab2_estim  <- estim_abundance(tab2, surf = 0.25, n_cores = 4, progress = FALSE)
-dat2        <- estim_summary(tab2, tab2_estim, surf)
-
-ggplot(dat2, aes(x = real, y = esti)) +
-  geom_point(size = 2) +
-  xlim(0, 2) +
-  ylim(0, 2) +
-  ggtitle("Estimations Vs. Réelles pour relevés en base 2") +
-  geom_abline(slope = 1, intercept = 0)
-ggsave("~/desktop/graphbase2.pdf")
-
-
-dat02 <- dat0[!(is.infinite(dat0$esti)),]
-dat22 <- dat2[!(is.infinite(dat0$esti)),]
-
-cor.test(dat22$esti, dat22$real)
-cor.test(dat02$esti, dat02$real)
+  geom_abline(slope = 1, intercept = 0) +
+  xlim(0, 40) +
+  ylim(0, 40) +
+  xlab("Real values") +
+  ylab("Estimates") +
+  facet_grid(periode ~ base) +
+  geom_text(x = 32, y = 20, aes(label = eqn), data = f_labels,
+            colour = "red", fontface = "bold") +
+  geom_text(x = 32, y = 15, aes(label = r2), data = f_labels,
+            colour = "red", fontface = "bold")
