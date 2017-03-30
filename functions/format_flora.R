13670fef1ae32b9548bb55f0f8361b3432dbc7c9
Xavier Laviron <xavier.laviron@gmx.fr>
2017-03-30 13:59:43 +0200

    put code to create trnasposed data frame in a function
    The function is in 'R/function/format_flora.R'. This way, I can easily apply it
    to other years.

---
+++ functions/format_flora.R
@@ -0,0 +1,44 @@
+transpose_df <- function(tab, n_quadras, n_subqd = 1, pos = "") {
+
+  # get rid of factors in 'tab'
+  i <- sapply(tab, is.factor)
+  tab[i] <- lapply(tab[i], as.character)
+
+  # create empty matrix to store abundance data
+  n_sp <- length(unique(tab$sp))
+  n_pl <- length(unique(tab$carre.parc))
+  nrowA <- n_pl * n_sp * length(pos)
+  ncolA <- n_quadras * n_subqd
+  A <- matrix(0, nrowA, ncolA)
+
+  # generate quadras names
+  qd <- rep(paste("q", 1:nquadras, sep = ""), rep(n_subqd, 10))
+  if (n_subqd > 1) {
+    colnames(A) <- paste(qd, letters[1:n_subqd], sep = "")
+  }
+
+  # create ids to get the line number
+  sp <- rep(unique(tab$sp), n_pl * length(pos))
+
+  # create a 'carre.parc' variable
+  parc <- unique(tab$carre.parc)
+  carre.parc <- rep(parc, rep(n_sp, length(parc)))
+
+  # create a 'position' variable
+  position <- rep(pos, rep(length(unique(tab$sp)), length(pos)))
+  position <- rep(position, length(parc))
+
+  # create single 'ids' variable to get row numbers
+  ids <- paste(sp, carre.parc, position)
+
+  for (i in 1:nrow(tab)) {
+    iid <- which(ids == paste(tab$sp[i], tab$carre.parc[i], tab$position[i]))
+    iqd <- paste("q", tab$plot[i], tab$quadra[i], sep = "")
+    A[iid, iqd] <- tab$abondance[i]
+  }
+
+  # bind ids + data
+  A <- cbind.data.frame(sp, carre.parc, position, A, stringsAsFactors = FALSE)
+
+  return(A)
+}
