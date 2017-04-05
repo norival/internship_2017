387f9d0dcb4d4e0b3fd30a6451dce9b60e280329
Xavier Laviron <xavier.laviron@gmx.fr>
2017-04-05 10:17:34 +0200

    adds parc name to return dfs of 'tidy_lda' function

--- functions/lda.R
+++ functions/lda.R
@@ -5,22 +5,28 @@ tidy_lda_post <- function(.post) {
 
   # group the data in a tidy manner so it can be easily plotted
   ## plots data
-  .all_plots <- data.frame(x = numeric(), val = numeric(), id = character())
+  .all_plots <-
+    data.frame(parc = character(), x = numeric(), val = numeric(), id = character())
   for (i in 1:ncol(.plots)) {
     .tmp <-
-      cbind.data.frame(1:nrow(.plots), .plots[, i], paste("gp", i, sep = ""))
+      cbind.data.frame(rownames(.plots), 1:nrow(.plots), .plots[, i], paste("gp", i, sep = ""))
     .all_plots <- rbind.data.frame(.all_plots, .tmp)
   }
-  colnames(.all_plots) <- c("x", "val", "gp")
+  colnames(.all_plots) <- c("parc", "x", "val", "gp")
+  .all_plots$parc <- as.character(.all_plots$parc)
+  .all_plots$gp   <- as.character(.all_plots$gp)
 
   ## species data
-  .all_spp <- data.frame(x = numeric(), val = numeric(), id = character())
+  .all_spp <-
+    data.frame(sp = character(), x = numeric(), val = numeric(), id = character())
   for (i in 1:nrow(.spp)) {
     .tmp <-
-      cbind.data.frame(1:ncol(.spp), .spp[i, ], paste("gp", i, sep = ""))
+      cbind.data.frame(rownames(.spp), 1:ncol(.spp), .spp[i, ], paste("gp", i, sep = ""))
     .all_spp <- rbind.data.frame(.all_spp, .tmp)
   }
-  colnames(.all_spp) <- c("x", "rel_ab", "gp")
+  colnames(.all_spp) <- c("sp", "x", "rel_ab", "gp")
+  .all_spp$sp <- as.character(.all_spp$sp)
+  .all_spp$gp <- as.character(.all_spp$gp)
 
   .tidy_dat <- list(plots = .all_plots, spp = .all_spp)
 
