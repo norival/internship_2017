b57fe3bb45ed76f34f907bdb4e17eee481c23cb8
Xavier Laviron <xavier.laviron@gmx.fr>
2017-04-12 18:20:52 +0200

    remove the code for older verifications
    I removed the code used for estimations based on base10 data.

--- R/functions/transpose_flora.R
+++ R/functions/transpose_flora.R
@@ -1,135 +1,11 @@
-transpose_flora <- function(tab, base = 0) {
-  # this function returns 2 dataframes:
-  #   origin    : data transposed to the same format as monitoring data so the
+transpose_flora_tot <- function(tab) {
+  # this function returns 4 dataframes:
+  #   orig      : data transposed to the same format as monitoring data so the
   #               functions to compute abundance can be used
-  #   converted : data transposed to the same format but with the values
-  #               converted into whether base 0 or base2 format
+  #   basen     : data transposed to the same format but with the values
+  #               converted into whether base0, base 10 or base2 format
   # The 'position' column is used to set compatibility with the estimations
   # functions.
-
-  # Prepare an empty matrix filled with 0 (for 0 abundance observed)
-  names <- unique(paste(tab$Zone, tab$Quadra, tab$Placette, sep = ""))
-  nrowA <- length(unique(tab$Parcelle)) * length(unique(tab$Taxon))
-  ncolA <- length(names) + 3
-  A <- matrix(ncol = ncolA, nrow = nrowA , data = rep(0, ncolA*nrowA))
-  A <- data.frame(A)
-
-  colnames(A) <- c("sp", "carre.parc", "position", names)
-
-  A$sp <- rep(unique(tab$Taxon), length(unique(tab$Parcelle)))
-  A$position <- "phantom"
-
-  # create a carre.parc variable
-  A$carre.parc <-
-    rep(unique(tab$Parcelle),
-        rep(length(unique(A$sp)), length(unique(tab$Parcelle))))
-
-  # fill in the empty quadras
-  for (i in 1:nrow(tab)) {
-    sp        <- tab[i, "Taxon"]
-    field     <- tab[i, "Parcelle"]
-    zone      <- tab[i, "Zone"]
-    quadrat   <- tab[i, "Quadrat"]
-    subqd     <- tab[i, "Placette"]
-    abondance <- tab[i, "Note"]
-    code      <- paste(zone, quadrat, subqd, sep = "")
-
-    A[A$sp == sp & A$carre.parc == field, colnames(A) == code] <- abondance
-  }
-
-  # create a dataframe converted according to the 'base' argument
-  x <- A[, 4:length(A)]
-  if (base == 0)
-    x[x > 1] <- 1
-  if (base == 2)
-    x[x > 2] <- 2
-
-  conv <- cbind.data.frame(A[, 1:3], x)
-
-  return(list(origin = A, converted = conv))
-
-}
-
-# ------------------------------------------------------------------------------
-
-estim_summary <- function(tab, tab_estim, surf) {
-
-  # replace very small estimate values with 0
-  tab_estim[tab_estim <= 1e-05] <- 0
-
-  func <- function(x) {
-    # convert abundance indices by taking the geometric mean for each class
-    x[x == 2] <- (2+9)/2
-    x[x == 3] <- (10+99)/2
-    x[x == 4] <- (100+999)/2
-    x[x == 5] <- (1000+9999)/2
-
-    return(x)
-  }
-
-  # keep the original tab before converting it
-  taborig <- tab
-
-  # replace log10 values with the geometric mean of the class and divide the
-  # results by the surface of one subquadra (0.25^2)
-  tab <-
-    tab %>%
-    mutate_if(is.numeric, funs(func))
-
-  # create empty matrix to store results: one line per plot and one column per
-  # species
-  mat_vide <- matrix(Inf,
-                     ncol = length(unique(tab$sp)),
-                     nrow = length(unique(tab$carre.parc)))
-  abond_per_plot <- as.data.frame(mat_vide)
-
-  colnames(abond_per_plot) <- unique(tab$sp)
-  rownames(abond_per_plot) <- unique(tab$carre.parc)
-
-  for (parc in unique(tab$carre.parc)) {
-    # do it for each plot
-    tab_parc <- tab[tab$carre.parc == parc,]
-
-    for (sp in unique(tab_parc$sp)) {
-      # compute the sum for each species and report to the sampled surface,
-      # which is surf * number of points
-      tab_sp <- tab_parc[tab_parc$sp == sp,]
-      ab <- sum(tab_sp[4:length(tab_sp)]) / (surf * (length(tab_sp) - 3))
-      abond_per_plot[parc, sp] <- ab
-    }
-  }
-
-  mat_vide <- matrix(0, ncol = 9, nrow = nrow(abond_per_plot) * ncol(abond_per_plot))
-  dat <- data.frame(mat_vide)
-  colnames(dat) <- c("parc", "sp", "real", "estimate", paste("n", 0:4, sep = ""))
-
-  # create table with 2 column: one for the real value and one for the estimate
-  # value
-  i <- 0
-  for (parc in rownames(abond_per_plot)) {
-    for (sp in colnames(abond_per_plot)) {
-      i <- i+1
-      orig <- as.numeric(taborig[taborig$carre.parc == parc & taborig$sp == sp, 4:length(taborig)])
-      dat$n0[i]       <- length(orig[orig == 0])
-      dat$n1[i]       <- length(orig[orig == 1])
-      dat$n2[i]       <- length(orig[orig == 2])
-      dat$n3[i]       <- length(orig[orig == 3])
-      dat$n4[i]       <- length(orig[orig == 4])
-      dat$sp[i]       <- sp
-      dat$parc[i]     <- parc
-      dat$real[i]     <- abond_per_plot[parc, sp]
-      dat$estimate[i] <- tab_estim[parc, sp]
-    }
-  }
-
-  return(dat)
-}
-
-# ------------------------------------------------------------------------------
-
-transpose_flora_tot <- function(tab) {
-  # this function returns 4 tables transposed in the right way for abundance
-  # estimation
 
   # Prepare an empty matrix filled with 0 (for 0 abundance observed)
   sp    <- unique(colnames(tab[, 16:length(tab)]))
@@ -180,7 +56,7 @@ transpose_flora_tot <- function(tab) {
 
 # ------------------------------------------------------------------------------
 
-estim_summary_tot <- function(tab, tab_estim, surf) {
+estim_summary <- function(tab, tab_estim, surf) {
 
   # replace very small estimate values with 0
   tab_estim[tab_estim <= min(tab_estim)] <- 0
@@ -215,7 +91,7 @@ estim_summary_tot <- function(tab, tab_estim, surf) {
 
 # ------------------------------------------------------------------------------
 
-estim_summary_tot_gm <- function(tab, tabgm, surf) {
+estim_summary_gm <- function(tab, tabgm, surf) {
 
   mat_vide <- matrix(0, ncol = 4, nrow = nrow(tab))
   dat <- data.frame(mat_vide)
--- R/verif_ab.R
+++ R/verif_ab.R
@@ -5,92 +5,6 @@ source("functions/abundance.R", encoding = "utf8")
 
 
 # -- data ----------------------------------------------------------------------
-flore1 <- read.csv("data/raw/manip_ble_2014_p1.csv",
-                   encoding = "utf8", stringsAsFactor = FALSE)
-flore2 <- read.csv("data/raw/manip_ble_2014_p2.csv",
-                   encoding = "utf8", stringsAsFactor = FALSE)
-flore3 <- read.csv("data/raw/manip_ble_2014_p3.csv",
-                   encoding = "utf8", stringsAsFactor = FALSE)
-
-flore <- rbind(flore1, flore2, flore3)
-
-
-# -- check estimations ---------------------------------------------------------
-# Check the estimates against the real values
-# The values are converted whether in base 0 (0/1) or base2 and then the
-# abundance is estimated like for the other samples.
-surf <- 0.25
-
-dat <- as.data.frame(matrix(nrow = 0, ncol = 8, 0))
-for (per in 1:3) {
-  cat("Computing period ", per, "...\n", sep = "")
-  tab <- flore[flore$Periode == per,]
-
-  for (base in c(0, 2)) {
-    cat(" Computing base ", base, "...\n", sep = "")
-    # transpose the dataframe into the same format than the monitoring
-    # dataframes so the functions to estimate abundances can be used
-    tabt    <- transpose_flora(tab, base = base)
-    origin  <- tabt[["origin"]]
-    conv    <- tabt[["converted"]]
-
-    # use the appropriate function to estimate the abundance of converted
-    # dataframes, according to 'base'
-    if (base == 0) {
-      ab <- estim_abundance01(conv, surf, progress = FALSE, addpos = FALSE)
-    } else {
-      ab <- estim_abundance(conv, surf, n_cores = 4, progress = FALSE, addpos = FALSE)
-    }
-
-    estim <- estim_summary(origin, ab, surf)
-    estim$periode <- paste("period", per, sep = "")
-    estim$base    <- paste("base", base, sep = "")
-
-    # removes infinite values to compute a linear model on the data
-    estim_noinf <- estim[!(is.infinite(estim$esti)),]
-
-    # fit a linear model on the data, and get the R^2 and the equation of the
-    # model to print those on the graphics
-    mod <- lm(estimate ~ real, data = estim_noinf)
-    estim$r2 <- summary(mod)$adj.r.squared
-    mod$coef <- round(mod$coef, 3)
-    estim$eqn <- paste("Y = ", mod$coef[2], "X + ", mod$coef[1], sep = "")
-
-    dat <- rbind.data.frame(dat, estim)
-  }
-}
-dat_noinf <- dat[!(is.infinite(dat$esti)),]
-
-# aggregate labels to be displayed on the plot
-f_labels <- aggregate(data.frame(r2 = dat$r2, eqn = dat$eqn),
-                      list(periode = dat$periode, base = dat$base),
-                      unique)
-f_labels$r2 <- paste("R2=", round(f_labels$r2, 3), sep = "")
-
-# plot dat!
-p <- ggplot(dat, aes(x = real, y = estimate)) +
-  geom_point(size = 1) +
-  geom_abline(slope = 1, intercept = 0) +
-  xlim(0, 40) +
-  ylim(0, 40) +
-  xlab("Real values") +
-  ylab("Estimates") +
-  facet_grid(periode ~ base) +
-  geom_text(x = 32, y = 20, aes(label = eqn), data = f_labels,
-            colour = "red", fontface = "bold") +
-  geom_text(x = 32, y = 15, aes(label = r2), data = f_labels,
-            colour = "red", fontface = "bold")
-
-# plots that are not good
-notgood <- dat[dat$estimate > 300 & dat$base == "base2" & dat$periode == "period2",
-               -seq(from = length(dat), to = length(dat) - 4, by = -1)]
-
-save.image('/tmp/data_verif.RData')
-
-
-# ------------------------------------------------------------------------------
-# with complete abundance
-
 flore <- read.csv("data/raw/flore_tot_per1.csv", encoding = "latin1", sep = ";",
                   stringsAsFactors = FALSE)
 
@@ -102,6 +16,13 @@ flore2013 <-
   flore2013[which(apply(flore2013[, 16:ncol(flore2013)], 1, function(x) sum(is.na(x)))
                   != ncol(flore2013) - 15),]
 
+
+# -- check estimations ---------------------------------------------------------
+# Check the estimates against the real values
+# The values are converted whether in base 0 (0/1) or base2 or base10 and then
+# the abundance is estimated like for the other samples.
+surf <- 1
+
 # transpose and convert the dataframe
 transposed <- transpose_flora_tot(flore2013)
 
@@ -112,12 +33,12 @@ gmean[gmean == 3] <- exp(mean(log(c(10, 99))))
 gmean[gmean == 4] <- exp(mean(log(c(100, 999))))
 gmean[gmean == 5] <- exp(mean(log(c(1000, 9999))))
 
-cor_gmean <- estim_summary_tot_gm(tab = transposed[["orig"]], gmean, surf = 1)
+cor_gmean <- estim_summary_gm(tab = transposed[["orig"]], gmean, surf = 1)
 
 ## test on base2
 # compute estimtions
 estim2 <- estim_abundance(x = transposed[["base2"]], surf = 1, n_cores = 4, addpos = FALSE)
 
-cor_base2 <- estim_summary_tot(transposed[["orig"]], estim2, surf = 1)
+cor_base2 <- estim_summary(transposed[["orig"]], estim2, surf = 1)
 
 save.image('/tmp/data_verif.RData')
