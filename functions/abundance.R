01078692a2d4a8e9989ff1aa2400b737030f4088
Xavier Laviron <xavier.laviron@gmx.fr>
2017-05-12 09:54:24 +0200

    adds a fun argument to the optimized function
    Adds an arument to choose which function use for estimation.

--- functions/abundance.R
+++ functions/abundance.R
@@ -1,4 +1,8 @@
-h.fct <- function(ltheta,v=v) {
+# ------------------------------------------------------------------------------
+# core estimations functions
+# ------------------------------------------------------------------------------
+
+h.fct <- function(ltheta, v = v) {
   # calcule la vraissemblance des données des quadras
   # v = observations
   # ltheta = log du paramètre de poisson (intensité)
@@ -20,8 +24,10 @@ h.fct <- function(ltheta,v=v) {
   return(ll)
 }
 
+# ------------------------------------------------------------------------------
+
 lgpoisson <- function(k, r, theta) {
-  # log-gamma-posson mixture law:
+  # log-gamma-poisson mixture law:
   # - 'r' and 'theta': paramaters for the gamma law
   # - value: return the log-likelihood 'ln P(X=k)'
 
@@ -34,8 +40,10 @@ lgpoisson <- function(k, r, theta) {
   return(log(pk))
 }
 
-gammapoisson <- function(param, v, maxtheta = 30) {
-  # on définit ensuite la fonction à minimiser:
+# ------------------------------------------------------------------------------
+
+log_lik_gpoisson <- function(param, v, maxtheta = 30) {
+  # get the log likelihood of the lgpoisson function
   # the function to minimize when estimating with Poisson-Gamma distribution
 
   # 'param' are given as log:
@@ -54,104 +62,155 @@ gammapoisson <- function(param, v, maxtheta = 30) {
 
   lp <- c(lp0, lp1, lp2)
 
-  # log-vraissemblance du vecteur d'abondances (somme des log-vraissemblances de
-  # chaque abondance). On mutliplie par '-1' car on cherche à minimiser la
-  # fonction
-  # log-likelihood of abundance vector (sum of all likelihoods) * -1 because the
-  # function is minimized:
+  # log-likelihood of abundance vector (sum of all log-likelihoods) * -1 because
+  # the function is minimized:
   ll <- (-1)*sum(lp[v+1])
 
   return(ll)
 }
 
-estim_abundance <- function(x, surf, n_cores = 2, progress = TRUE, addpos = TRUE,
-                            fun = "h.fct", maxtheta = 30, nopar = FALSE) {
+# ------------------------------------------------------------------------------
 
-  if (!nopar) library(doSNOW)
+estim_core_gpoisson <- function(v, maxtheta) {
+  # core function to estimate abundance from the gamma-poisson mixture law (or
+  # negative binomial). It minimises the function log_lik_gpoisson, computes the
+  # mean and returns it
+  Zu <- nlminb(c(0, 0), log_lik_gpoisson, v = v, maxtheta = maxtheta,
+               lower = c(-50, -50), upper = c(50, 50),
+               control = list(itermax = 1000, abstol = 1e-20))
 
-  if (addpos & sum(grepl("Pa|In", x$carre.parc)) != length(x$carre.parc)) {
-    # adds the position to the name of the plot, like in the old days
-    x$carre.parc <- paste(x$carre.parc, stringr::str_to_title(x$pos), sep = "-")
+  r  <- exp(Zu$par[1])
+  p  <- 1 / (exp(Zu$par[2]) + 1)
+  mm <- r * ((1 - p) / p)
+
+  return(mm)
+}
+
+# ------------------------------------------------------------------------------
+
+estim_core_compoisson <- function(v, maxtheta) {
+  # core function to estimate abundance from the com-poisson distribution.
+  # It minimises the function h.fct, computes the
+  # mean and returns it
+  library(compoisson)
+
+  Zu <- nlminb(c(0, 0), h.fct, v = v1, maxtheta = maxtheta,
+               lower = c(-50, -50), upper = c(50, 50),
+               control = list(itermax = 1000, abstol = 1e-20))
+
+  mm <- com.mean(exp(Zu$par[1]), exp(Zu$par[2]))
+
+  return(mm)
+}
+
+# ------------------------------------------------------------------------------
+# utilities functions
+# ------------------------------------------------------------------------------
+
+group_subqd <- function(x, base2 = TRUE, n.subqd = 4) {
+  # function to group subquadras when sample are of the form 0/1 with 4
+  # subquadras and convert it to 0/1/2
+  #  -> we consider there cannot be more than one plant per subquadra
+
+  mat <- x[, 4:length(x)]
+
+  newmat <- matrix(nrow = nrow(mat), ncol = ncol(mat) / n.subqd, 0)
+
+  for(i in 1:ncol(newmat)) {
+    # compte tous les sous quadras
+    ii <- 4*(i-1) + (1:4)
+    quadra <- as.numeric(rowSums(mat[, ii]))
+    if (base2) {
+      # si > 2, plusieurs plantes dans le quadra -> 2
+      quadra[quadra > 2] <- 2
+    } else {
+      # si > 1, plusieurs plantes dans le quadra -> 1
+      quadra[quadra > 1] <- 1
+    }
+    newmat[,i] <- quadra
   }
 
-  abond_per_plot <- matrix(Inf,
+  colnames(newmat) <- paste0("q", 1:ncol(newmat))
+
+  return(cbind.data.frame(x[, 1:3], newmat))
+}
+
+# ------------------------------------------------------------------------------
+# wrapper functions
+# ------------------------------------------------------------------------------
+
+estim_abundance <- function(x, surf, n_cores = 2, progress = TRUE, addpos = TRUE,
+                            fun = "gammapoisson", maxtheta = 30, nopar = FALSE)
+{
+  # -- function to estimate abundances -----------------------------------------
+  # How it works:
+  # To increase speed, we first sort the abundances vectors to get vectors that
+  # are uniques (same abundance notes). The estimation function is then run only
+  # on those unique vectors (generally much less than the total number of
+  # vectors) and the result is stored. Finally, estimations for the whole
+  # dataframe are drawn from the few unique vectors estimations, and results are
+  # stored in a matrix site * species and returned
+  # ----------------------------------------------------------------------------
+
+  library(doSNOW)
+  options(stringsAsFactors = FALSE)
+
+  # build empty site*species matrix to store the final results
+  abond_per_plot <- matrix(0,
                            ncol = length(unique(x$sp)),
                            nrow = length(unique(x$carre.parc)))
 
   colnames(abond_per_plot) <- unique(x$sp)
   rownames(abond_per_plot) <- unique(x$carre.parc)
 
-  # add this if parallelization is not working (eg 'makeCluster()' hangs
-  # indefinitely), it uses the non parallelized function at the end of this file
-  # and exit the main function
-  if (nopar) {
-    out <- estim_nopar(x = x, out = abond_per_plot, surf = surf, fun = fun,
-                       maxtheta = maxtheta, progress = progress)
-    return(out)
-  }
-
-  if (progress) {
-    cat("Estimation of abundances\n")
-
-    pb  <- txtProgressBar(min = 0, max = nrow(x), style = 3, width = 80)
-    progressbar <- function(n) setTxtProgressBar(pb, n)
-    opts <- list(progress = progressbar)
-  } else {
-    opts <- list()
-  }
-
+  # start cluster
   cl <- makeCluster(n_cores)
   registerDoSNOW(cl)
+
+  # wee need to export the core functions to the cluster environnement
+  if (fun == "gammapoisson") {
+    clusterExport(cl, c("lgpoisson", "log_lik_gpoisson", "estim_core_gpoisson"))
+  }
+  if (fun == "compoisson") {
+    clusterExport(cl, c("h.fct", "estim_core_gpoisson"))
+  }
+
+  # remove plots with only '0'
+  x <- x[rowSums(x[,4:length(x)]) != 0, ]
+
+  # separate the numeric parts and the inofrmations part
+  infos <- as.matrix(x[,1:3])
+  tab   <- as.matrix(x[,4:length(x)])
+
+  # sort abundances vectors
+  tabsort <- t(apply(tab, 1, sort))
+
+  # get only unique vectors in tabsort
+  tabsort <- tabsort[!duplicated(tabsort),]
+
+  # collapse abundance vector to get an 'id' vector
+  ids <- as.character(apply(tabsort, 1, function(x) paste0(x, collapse = "")))
+
+  # estimate abundance on unique vectors only
+  if (fun == "gammapoisson") {
+    ab <- parApply(cl, tabsort, 1,
+                   function(x, maxtheta) estim_core_gpoisson(x, maxtheta),
+                   maxtheta)
+  } else if (fun == "compoisson") {
+    ab <- parApply(cl, tabsort, 1,
+                   function(x, maxtheta) estim_core_compoisson(x, maxtheta),
+                   maxtheta)
+  }
+
+  # now get estimates for the whole dataframe
+  ids_full <- as.character(parApply(cl, tab, 1, function(x) paste0(sort(x), collapse = "")))
+  ab_full  <- as.numeric(parSapply(cl, ids_full, function(x, ab, ids) ab[ids == x], ab, ids))
   
-  export <- c(fun, "lgpoisson")
-
-  a <- foreach(i = 1:nrow(x), .combine = rbind, .export = export, .options.snow = opts) %dopar% {
-
-    if (progress) setTxtProgressBar(pb, i)
-
-    # param de Poisson par espèce
-    # ici on récupère la ligne i, qui correspond à une espèce pour une parcelle
-    v1 <- as.numeric(x[i, 4:ncol(x)])
-
-    if (fun == "h.fct") {
-      # on estime l'abondance de la parcelle par la loi de poisson
-      Zu <- nlminb(c(0, 0), h.fct, v = v1, lower = c(-50, -50), upper = c(50, 50))
-
-      # on fait la moyenne de poisson sur le paramètre de Zu, qui correspond aux
-      # moyennes. On repasse en exponentielle car on avait fait un log
-      mm <- com.mean(exp(Zu$par[1]), exp(Zu$par[2]))
-
-    } else if (fun == "gammapoisson") {
-      Zu <- nlminb(c(0, 0), gammapoisson, v = v1, maxtheta = maxtheta,
-                   lower = c(-50, -50), upper = c(50, 50),
-                   control = list(iter.max = 1000, abs.tol = 1e-20))
-
-      r <- exp(Zu$par[1])
-      p <- 1 / (exp(Zu$par[2]) + 1)
-      mm <- r * ((1 - p) / p)
-    }
-
-    cbind.data.frame(x$carre.parc[i], x$sp[i], mm,
-                     stringsAsFactors = F)
-  }
-
-  # On remplit la matrice vide avec les valeurs de a
-  if (progress) {
-    cat("\nFormating results\n")
-    pb  <- txtProgressBar(min = 0, max = nrow(a), style = 3, width = 80)
-  }
+  a <- cbind(infos[,1:2], ab_full)
 
   for (i in 1:nrow(a)) {
-    if (progress) {
-      setTxtProgressBar(pb, i)
-    }
-    abond_per_plot[a[i, 1], a[i, 2]] <- a[i, 3]
+    abond_per_plot[a[i, 2], a[i, 1]] <- as.numeric(a[i, 3]) / surf
   }
-
-  if (progress) close(pb)
-
-  # rapporter à 1 m^2
-  abond_per_plot <- abond_per_plot / surf
 
   stopCluster(cl)
 
@@ -159,12 +218,12 @@ estim_abundance <- function(x, surf, n_cores = 2, progress = TRUE, addpos = TRUE
 }
 
 # ------------------------------------------------------------------------------
-# This one is not used anymore for estimations but is used by the script that
-# checks estimations
 
 estim_abundance01 <- function(x, surf, gp.subquadra = FALSE, base2 = FALSE,
                               progress = TRUE, addpos = TRUE, fun = "h.fct") {
   # estimates abundance when data is 0/1
+  # This one is not used anymore for estimations but is used by the script that
+  # checks estimations
 
   if (addpos & sum(grepl("Pa|In", x$carre.parc)) == 0) {
     # adds the position to the name of the plot, like in the old days
@@ -257,36 +316,6 @@ estim_abundance01 <- function(x, surf, gp.subquadra = FALSE, base2 = FALSE,
 }
 
 # ------------------------------------------------------------------------------
-
-group_subqd <- function(x, base2 = TRUE, n.subqd = 4) {
-  # x <- data2016
-  # n.subqd <- 4
-
-  mat <- x[, 4:length(x)]
-
-  newmat <- matrix(nrow = nrow(mat), ncol = ncol(mat) / n.subqd, 0)
-
-  for(i in 1:ncol(newmat)) {
-    # compte tous les sous quadras
-    ii <- 4*(i-1) + (1:4)
-    quadra <- as.numeric(rowSums(mat[, ii]))
-    if (base2) {
-      # si > 2, plusieurs plantes dans le quadra -> 2
-      quadra[quadra > 2] <- 2
-    } else {
-      # si > 1, plusieurs plantes dans le quadra -> 1
-      quadra[quadra > 1] <- 1
-    }
-    newmat[,i] <- quadra
-  }
-
-  colnames(newmat) <- paste0("q", 1:ncol(newmat))
-
-  return(cbind.data.frame(x[, 1:3], newmat))
-}
-
-
-# ------------------------------------------------------------------------------
 # this is just a non-parellelized version of the part that do the estimation,
 # used when 'nopar' arg is set to TRUE.
 
@@ -326,79 +355,3 @@ estim_nopar <- function(x, out, surf, fun, maxtheta, progress) {
   return(out)
 }
 
-# ------------------------------------------------------------------------------
-
-estim_abundance2 <- function(x, surf, n_cores = 2, progress = TRUE, addpos = TRUE,
-                             fun = "h.fct", maxtheta = 30, nopar = FALSE)
-{
-  # fonction to estimate abundances
-
-  library(doSNOW)
-  options(stringsAsFactors = FALSE)
-  # ----------------------------------------------------------------------------
-  # x <- data2006
-  # maxtheta <- 20
-  # n_cores <- 3
-  # surf <- 4
-  # ----------------------------------------------------------------------------
-
-  estim_core <- function(.v, .maxtheta) {
-    .Zu <- nlminb(c(0, 0), gammapoisson, v = .v, maxtheta = .maxtheta,
-                 lower = c(-50, -50), upper = c(50, 50),
-                 control = list(iter.max = 1000, abs.tol = 1e-20))
-
-    .r <- exp(.Zu$par[1])
-    .p <- 1 / (exp(.Zu$par[2]) + 1)
-    .mm <- .r * ((1 - .p) / .p)
-
-    return(.mm)
-  }
-
-  # build empty matrix to store the final results
-  abond_per_plot <- matrix(0,
-                           ncol = length(unique(x$sp)),
-                           nrow = length(unique(x$carre.parc)))
-
-  colnames(abond_per_plot) <- unique(x$sp)
-  rownames(abond_per_plot) <- unique(x$carre.parc)
-
-  cl <- makeCluster(n_cores)
-  registerDoSNOW(cl)
-
-  clusterExport(cl, c("lgpoisson", "gammapoisson", "estim_core"))
-
-  # remove plots with only '0'
-  x <- x[rowSums(x[,4:length(x)]) != 0, ]
-
-  # separate the numeric parts and the inofrmations part
-  infos <- as.matrix(x[,1:3])
-  tab   <- as.matrix(x[,4:length(x)])
-
-  # sort abundances vectors
-  tabsort <- t(apply(tab, 1, sort))
-
-  # get only unique vectors in tabsort
-  tabsort <- tabsort[!duplicated(tabsort),]
-
-  # collapse abundance vector to get an 'id' vector
-  ids <- as.character(apply(tabsort, 1, function(x) paste0(x, collapse = "")))
-
-  # estimate abundance on unique vectors only
-  ab <- parApply(cl, tabsort, 1,
-                 function(x, maxtheta) estim_core(x, maxtheta),
-                 maxtheta)
-
-  # now get estimates for the whole dataframe
-  ids_full <- as.character(parApply(cl, tab, 1, function(x) paste0(sort(x), collapse = "")))
-  ab_full  <- as.numeric(parSapply(cl, ids_full, function(x, ab, ids) ab[ids == x], ab, ids))
-  
-  a <- cbind.data.frame(infos[,1:2], ab_full)
-
-  for (i in 1:nrow(a)) {
-    abond_per_plot[a[i, 2], a[i, 1]] <- a[i, 3] / surf
-  }
-
-  stopCluster(cl)
-
-  return(abond_per_plot)
-}
