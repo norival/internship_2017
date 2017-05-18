b0655dc595239abb60507ea72b242bae96e5a703
Xavier Laviron <xavier.laviron@gmx.fr>
2017-05-18 15:21:04 +0200

    adds LDA fitting by threshold

--- lda/lda_fitting.R
+++ lda/lda_fitting.R
@@ -32,3 +32,22 @@ for (year in 2006:2016) {
             datname = paste0("lda_year_", year, "_K_groups"),
             path    = "data/generated")
 }
+
+
+# lda models on all years but with different thresholds
+control <- list(seed = 42, burnin = 10000, thin = 500, iter = 50000)
+
+lda_by_thrs <- list()
+
+for (thrs in c(0.6, 0.5, 0.4, 0.3, 0.2, 0.01)) {
+  species  <- apply(dat, 2, function(x) sum(x > 0) / length(x))
+
+  # select only species that occur in at least thrs % of plots
+  dat_thrs <- dat[,which(species > thrs)]
+  dat_thrs <- dat_thrs[rowSums(dat_thrs) != 0,]
+
+  lda_by_thrs[[as.character(thrs)]] <-
+    par_lda(tab = dat_thrs, groups = 11:15, control = control, ncores = 3,
+            datname = paste0("lda_", thrs * 100, "_percent_K_groups"),
+            path    = "data/generated")
+}
